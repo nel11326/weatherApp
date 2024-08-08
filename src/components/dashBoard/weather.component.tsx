6be725eda154de985dcdf27eb35abdce5cf59bec
nel11326 <lenor11326@hotmail.com>
2024-08-08 19:21:47 +0800

    feat(dashboard): add weather app api and logic

--- src/components/dashBoard/weather.component.tsx
+++ src/components/dashBoard/weather.component.tsx
@@ -1,10 +1,36 @@
+import { ChangeEvent, useState } from "react";
 import "./weather.component.css";
 
-export const WeatherApp = () => {
+export const WeatherApp = (): JSX.Element => {
+  const [location, setLocation] = useState<string>("");
+  const [options, setOptions] = useState<[]>([]);
+
+  const getSearchOptions = async (value: string) => {
+    try {
+      const response = await fetch(
+        `http://api.openweathermap.org/geo/1.0/direct?q=${value.trim()}&limit=5&appid=${
+          process.env.REACT_APP_API_KEY
+        }`
+      );
+      const data = await response.json();
+      setOptions(data);
+    } catch (error: any) {
+      console.log(error.message);
+    }
+  };
+  const onInputChange = (event: ChangeEvent<HTMLInputElement>) => {
+    const value = event.target.value.trim();
+    setLocation(value);
+
+    if (value === "") return;
+
+    getSearchOptions(value);
+  };
+
   return (
     <div className="dashboard col-md-6">
-      <div className="card col-md-6">
-        <h1 className="font-thin">
+      <div className="card">
+        <h1>
           Weather <span className="font-bold">ForeCast</span>
         </h1>
         <p>
@@ -12,7 +38,20 @@ export const WeatherApp = () => {
           option from the dropdown
         </p>
         <div className="flex">
-          <input type="text" placeholder="Enter City" />
+          <input
+            type="text"
+            placeholder="Enter City"
+            value={location}
+            onChange={onInputChange}
+          />
+          <ul>
+            {options.map((option: { name: string }, index: number) => (
+              <li key={option.name + "-" + index}>
+                {" "}
+                <button>{option.name}</button>
+              </li>
+            ))}
+          </ul>
           <button>Search</button>
         </div>
       </div>
